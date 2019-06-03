63087cc722d5ef231ec0c9cc13a23864bbac9608
Øyvind Hagen <oyvind.hagen@nav.no>
2019-06-03 13:38:12 +0200

    Lagt til selftest mot database.

--- src/main/kotlin/no/nav/pam/xmlstilling/admin/platform/Health.kt
+++ src/main/kotlin/no/nav/pam/xmlstilling/admin/platform/Health.kt
@@ -5,6 +5,10 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import kotliquery.HikariCP
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import kotliquery.using
 
 fun Routing.health(
         ready: () -> Boolean = { true },
@@ -13,12 +17,23 @@ fun Routing.health(
 
     fun statusFor(b: () -> Boolean) = b().let{ if(it) HttpStatusCode.OK else HttpStatusCode.InternalServerError}
 
+    fun databaseConnectionOk(): String {
+        try {using(sessionOf(HikariCP.dataSource())) {
+            it.run(queryOf("select now()").asExecute)
+        }} catch(e: Exception) {return "NOT OK"}
+        return "OK"
+    }
+
     get("isReady") {
         statusFor(ready).let { call.respondText("Ready: $it", status = it) }
     }
 
     get("isAlive") {
         statusFor(alive).let { call.respondText("Alive: $it", status = it) }
+    }
+
+    get("internal/selftest") {
+        databaseConnectionOk().let {call.respondText { "Database $it"}}
     }
 
 }
